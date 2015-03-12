3a0cec13fb6cfc56451b8f40af4ab9096572d409
Jesse <jesserbrightman@gmail.com>
2015-03-12 00:28:36 -0700

    tried to fix but not working

--- js/app.js
+++ js/app.js
@@ -3,34 +3,14 @@
  */
 
 // Declare module that references our controllers.
-var loginApp = angular.module('loginApp', ['ngRoute', 'loginAppControllers']).config(function ($routeProvider) {
+var loginApp = angular.module('loginApp', ['ngRoute', 'myControllers'])
+    .config(function ($routeProvider) {
+        $routeProvider.when("/", {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+        })
 
-    /*
-     Inject the AngularJS routing (ngRoute) service so we can
-     access the $routeProvider reference in our routing function.
-     Also inject the 'cardAppControllers' service which we will
-     define in 'controllers.js'.
-     */
-
-    'use strict';
-
-    $routeProvider.when("/home", {
-        /* When 'home' route is selected
-         use the 'list.html' template and the 'ListCtrl' controller. */
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-    }).
-        /*when('/home/number/:numberID/suit/:suitID', {
-            templateUrl: 'views/detail.html', controller: 'DetailCtrl'
-        }).
-        when('/add', {
-            templateUrl: 'views/add.html', controller: 'AddCtrl'
-        }).
-        when('/addconfirm/number/:numberID/suit/:suitID', {
-            templateUrl: 'views/confirm.html', controller: 'AddConfirmCtrl'
-        }).*/
-
-        // If no route is selected then use the 'home' route.
-        otherwise({ redirectTo: '/home' });
-});
+            // If no route is selected then use the 'home' route.
+            .otherwise({ redirectTo: '/index' });
+    });
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,47 +1,15 @@
 /*global angular */
 
-var cardAppControllers = (function () {
-    'use strict';
-    var cardAppControllers = angular.module('cardAppControllers', []);
+// Controller - dispatches inputs and outputs.
+var myControllers = (function () {
+    var myControllers = angular.module('myControllers', []);
 
-    // Declare the application controller and inject the scope reference.
-    cardAppControllers.controller('AppCtrl', ['$scope', function ($scope) {
-        // Define the title model.
-        $scope.title = "AngularJS Tutorial";
+    // Controllers are defined by the controller function.
+    myControllers.controller('AppCtrl', ['$scope', function ($scope) {
+        $scope.title = "Login Assignment";
     }]);
-    // Inject the scope and new cardService reference into the controller.
-    cardAppControllers.controller('LoginCtrl', ['$scope', function ($scope) {
-        $scope.title = "Login Page";
-        }]);
-    // Inject scope, $routeParams, and cardService
-    cardAppControllers.controller('DetailCtrl', ['$scope', '$routeParams', 'cardService',
-        function ($scope, $routeParams, cardService) {
-            cardService.getCard($scope, $routeParams.numberID, $routeParams.suitID);
-        }]);
-    // AddConfirmCtrl calls the service to add the card.
-    cardAppControllers.controller('AddConfirmCtrl', ['$scope', '$routeParams', 'cardService',
-        function ($scope, $routeParams, cardService) {
-            cardService.addCard($scope, $routeParams.numberID, $routeParams.suitID);
-        }]);
-
-    // The $location service is injected to enable the redirect.
-    cardAppControllers.controller('AddCtrl', ['$scope', '$location', 'cardService',
-        function AddCtrl($scope, $location, cardService) {
-            // Create an empty object.
-            $scope.master = {};
-
-            // When add is clicked, redirect to the confirm view and controller where
-            // the card is actually added added.
-            $scope.add = function (card) {
-                $location.path('/addconfirm/number/' + card.number + '/suit/' + card.suit);
-            };
-            // When reset is clicked clear the ‘card’ model defined within the scope to
-            // clear the form data.
-            $scope.reset = function () {
-                $scope.card = angular.copy($scope.master);
-            };
-        }]);
-
-
-    return cardAppControllers;
+    myControllers.controller('LoginCtrl', ['$scope', function ($scope) {
+        $scope.title = "Login";
+    }]);
+    return myControllers;
 }());
