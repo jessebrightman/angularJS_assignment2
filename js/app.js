8141c8aec77e66a286ac3e1203676f778e9c33e8
Jesse <jesserbrightman@gmail.com>
2015-03-15 21:08:02 -0700

    updated validation

--- js/app.js
+++ js/app.js
@@ -2,12 +2,32 @@
  * Created by Jesse on 09/03/2015.
  */
 
+// declare angular app
+    var validationApp = angular.module('validationApp', []);
+
+// declare controller
+    validationApp.controller('mainController', function($scope){
+
+        //declare function for submit
+        $scope.submitForm = function(isValid, $location){
+            if(isValid){
+                $location.path("/welcome");
+            }
+        }
+
+    });
+
 // Declare module that references our controllers.
-var loginApp = angular.module('loginApp', ['ngRoute', 'myControllers'])
+var loginApp = angular.module('validationApp', ['ngRoute', 'myControllers'])
     .config(function ($routeProvider) {
         $routeProvider.when("/", {
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
+        }),
+
+        $routeProvider.when("/welcome", {
+            templateUrl: 'views/welcome.html',
+            controller: 'LoginCtrl'
         })
 
             // If no route is selected then use the 'home' route.
